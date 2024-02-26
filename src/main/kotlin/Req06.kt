d9fe79c3df6bdc09cff97f9fc6f0e70b6d8ae4c3
TatianeMarinho <devtatianemarinho@gmail.com>
2024-02-26 15:25:03 -0400

    req 06

--- src/main/kotlin/Req06.kt
+++ src/main/kotlin/Req06.kt
@@ -1,5 +1,24 @@
 fun calculateNewSalary(currentSalary: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+    when {
+        currentSalary <= 300.00 -> {
+            return currentSalary + (currentSalary * 0.50)
+        }
+        currentSalary in 300.01..500.00 -> {
+            return currentSalary + ( currentSalary * 0.40)
+        }
+        currentSalary in 500.01..700.00 -> {
+            return currentSalary + ( currentSalary * 0.30)
+        }
+        currentSalary in 700.01..800.00 -> {
+            return currentSalary + ( currentSalary * 0.20)
+        }
+        currentSalary in 800.01..1000.00 -> {
+            return currentSalary + ( currentSalary * 0.10)
+        }
+        else -> {
+            return currentSalary + ( currentSalary * 0.05)
+        }
+    }
 }
 
 fun main() {
