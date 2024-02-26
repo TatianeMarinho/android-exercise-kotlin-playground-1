5d584047c8650ecadfe713d606860770a32226d7
TatianeMarinho <devtatianemarinho@gmail.com>
2024-02-26 15:16:27 -0400

    req 05

--- src/main/kotlin/Req05.kt
+++ src/main/kotlin/Req05.kt
@@ -1,5 +1,15 @@
 fun calculateConsumerPrice(factoryCost: Double): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+    when {
+        factoryCost > 25000.00 -> {
+            return (factoryCost * 0.15) + (factoryCost * 0.20) + factoryCost
+        }
+        factoryCost in 12000.01..25000.00 -> {
+            return (factoryCost * 0.10) + (factoryCost * 0.15) + factoryCost
+        }
+        else -> {
+            return (factoryCost * 0.05) + factoryCost
+        }
+    }
 }
 
 fun main() {
