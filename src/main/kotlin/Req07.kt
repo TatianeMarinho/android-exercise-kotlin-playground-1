06508b486480fb3ebbe15f8a36d4941aa9a97048
TatianeMarinho <devtatianemarinho@gmail.com>
2024-02-26 16:07:47 -0400

    req 07

--- src/main/kotlin/Req07.kt
+++ src/main/kotlin/Req07.kt
@@ -1,13 +1,19 @@
 fun calculateAverage(ages: List<Int>): Double {
-    TODO("Use essa função para desenvolver sua lógica")
+    val total = ages.sum()
+    return total.toDouble() / ages.size
 }
 
 fun countPeopleWeightHeight(weights: List<Double>, heights: List<Double>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
+    return weights.zip(heights).count{ (weight, height) -> weight > 90 && height< 1.50 }
 }
 
 fun calculatePercentageAgeHeight(ages: List<Int>, heights: List<Double>): Int {
-    TODO("Use essa função para desenvolver sua lógica")
+    val countPerson = ages.count{ it in 11..29 }
+    val countHeight = heights.count{ it > 1.90}
+    val total = countHeight + countPerson
+    val size = ages.size + heights.size
+
+    return ((total.toDouble() / size.toDouble()) * 100).toInt()
 }
 
 fun main() {
